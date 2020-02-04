395202b7b729e4d3883287314f1c2be9338ab054
Chris Blackburn <thetootall@gmail.com>
2020-02-04 17:28:17 -0600

    Create Get-AdGroupMember.ps1

---
+++ Get-AdGroupMember.ps1
@@ -0,0 +1,33 @@
+#https://serverfault.com/questions/532945/list-all-groups-and-their-members-with-powershell-on-win2008r2
+
+Import-Module ActiveDirectory
+
+$Groups = Get-AdGroup -filter *  | select name,objectguid
+
+Foreach ($Group in $Groups) {
+Write-host $Group.name
+
+  $Arrayofmembers = Get-ADGroupMember -identity $Group.objectguid -recursive | select name,samaccountname,userprincipalname
+
+
+  foreach ($Member in $Arrayofmembers) {
+  $Table = @()
+
+$Record = @{
+  "Group Name" = ""
+  "Name" = ""
+  "Username" = ""
+  "UPN" = ""
+}
+    $Record."Group Name" = $Group.name
+    $Record."Name" = $Member.name
+    $Record."UserName" = $Member.samaccountname
+    Clear-Variable memberupn
+    $memberupn = Get-ADUser $Member.samaccountname | select userprincipalname
+    $Record."UPN" = $memberupn.userprincipalname
+    $objRecord = New-Object PSObject -property $Record
+    $Table += $objrecord
+   $Table | export-csv "SecurityGroups.csv" -NoTypeInformation -append
+  }
+}
+
